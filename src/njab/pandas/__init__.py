cf32a425753e6baf1814b65b6f35635109419a33
Henry <henry.webel@cpr.ku.dk>
2023-10-30 16:15:15 +0100

    :bug::art: specify missing dependency and format of file
    - long_discription: set format for pypi formatting
    - add missing dependency
    - format code according to yapf

--- src/njab/pandas/__init__.py
+++ src/njab/pandas/__init__.py
@@ -7,18 +7,23 @@ import omegaconf
 logger = logging.getLogger(__name__)
 
 
-def replace_with(string_key: str, replace: str = "()/", replace_with: str = '') -> str:
+def replace_with(string_key: str,
+                 replace: str = "()/",
+                 replace_with: str = '') -> str:
     """Replace characters in a string with a replacement."""
     for symbol in replace:
         string_key = string_key.replace(symbol, replace_with)
     return string_key
 
 
-def get_colums_accessor(df: pd.DataFrame, all_lower_case=False) -> omegaconf.OmegaConf:
+def get_colums_accessor(df: pd.DataFrame,
+                        all_lower_case=False) -> omegaconf.OmegaConf:
     """Get an dictionary augmented with attribute access of column name as key
        with white spaces replaced and the original column name as values."""
-    cols = {replace_with(col.replace(' ', '_').replace(
-        '-', '_')): col for col in df.columns}
+    cols = {
+        replace_with(col.replace(' ', '_').replace('-', '_')): col
+        for col in df.columns
+    }
     if all_lower_case:
         cols = {k.lower(): v for k, v in cols.items()}
     return omegaconf.OmegaConf.create(cols)
@@ -35,8 +40,7 @@ def col_isin_df(cols: typing.Union[list, str], df: pd.DataFrame) -> list:
     ret = list()
     for _var in cols:
         if _var not in df.columns:
-            logger.warning(
-                f"Desired variable not found: {_var}", stacklevel=0)
+            logger.warning(f"Desired variable not found: {_var}", stacklevel=0)
             continue
         ret.append(_var)
     return ret
